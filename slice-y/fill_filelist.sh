190af4cd3d1b51c11270a4748a07da0b6fba7450
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2018-02-28 16:39:40 +0000

    Standard run goes to T=50; reflected this in makefile

--- slice-y/fill_filelist.sh
+++ slice-y/fill_filelist.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 ISOTROPIC_DIR=/nas/1101974q/kink-instability-str8/mid-res/without-rampdown/v-4r-4-isotropic
 SWITCHING_DIR=/nas/1101974q/kink-instability-str8/mid-res/without-rampdown/v-4r-4-switching
-ls ${ISOTROPIC_DIR}/Data/*.sdf | sed -e 's/ /\\n/g' > filename_list
-ls ${SWITCHING_DIR}/Data/*.sdf | sed -e 's/ /\\n/g' > filename_list
+ls ${ISOTROPIC_DIR}/Data/*.sdf ${SWITCHING_DIR}/Data/*.sdf | sed -e 's/ /\\n/g' > filename_list
